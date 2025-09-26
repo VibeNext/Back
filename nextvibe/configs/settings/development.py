ad946f1005fa0281de5907ec199c05338e49db74
gyk <ganada1375@gmail.com>
2025-09-26 23:52:02 +0900

    🐛 Db링크 디폴트 설정

--- nextvibe/configs/settings/development.py
+++ nextvibe/configs/settings/development.py
@@ -4,12 +4,15 @@ import environ
 environ.Env.read_env(os.path.join(BASE_DIR, 'env', '.env.dev'))
 
 DATABASES = {
-    'default': env.db(),
+    "default": env.db(
+        "DATABASE_URL",
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
+    )
 }
 
-CACHES = {
-    'default': env.cache(),
-}
+# CACHES = {
+#    'default': env.cache(),
+#}
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
