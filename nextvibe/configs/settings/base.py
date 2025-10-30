6e20cf5a69f1369f3c0496af2c7ec0842c5f86af
Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
2025-10-31 02:48:29 +0900

    Reorder installed apps in base.py

--- nextvibe/configs/settings/base.py
+++ nextvibe/configs/settings/base.py
@@ -43,10 +43,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'users',
     'corsheaders',
+    'rest_framework',
     'rest_framework_simplejwt',
+    'users',
 ]
 
 MIDDLEWARE = [
