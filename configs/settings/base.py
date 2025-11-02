ec4327511074b48c065d8903885e9279069ac7c2
jasmin0202 <ganada1375@gmail.com>
2025-11-02 15:45:57 +0900

    fix: 폴더명 오타 수정
    Co-authored-by: Hyerin Sung <145767486+hyerindev@users.noreply.github.com>

--- configs/settings/base.py
+++ configs/settings/base.py
@@ -22,7 +22,7 @@ env = environ.Env(
     DEBUG = (bool, False),
     SECRET_KEY=(str, "dev-secret"),
 )
-environ.Env.read_env(os.path.join(BASE_DIR, '.env', '.env.base'))
+environ.Env.read_env(os.path.join(BASE_DIR, 'env', '.env.base'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
