b7716c0f552bbe1a1a44e627727c5766ccfc0c8e
gyk <ganada1375@gmail.com>
2025-11-02 21:48:46 +0900

    ✨ Sender choice 생성, message model 생성

--- solutions/models.py
+++ solutions/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class SolutionHistory(models.Model):
-    user=models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    user=models.ForeignKey('accounts.User', on_delete=models.CASCADE)
     mission=models.ForeignKey('missions.Mission', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     solved_at = models.DateTimeField(null=True, blank=True)
@@ -11,3 +11,23 @@ class SolutionHistory(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Sender(models.TextChoices):
+    USER = 'user', 'User'
+    AI = 'ai', 'AI'
+
+
+    
+class Message(models.Model):
+    solution_history = models.ForeignKey(SolutionHistory, on_delete=models.CASCADE, related_name='messages')
+    created_at = models.DateTimeField(auto_now_add=True)
+    sender = models.CharField(
+        max_length=10,
+        choices=Sender.choices,
+        default=Sender.USER
+    )
+    content = models.TextField()
+    
+
+    def __str__(self):
+        return f"Message from {self.sender} at {self.created_at}"
