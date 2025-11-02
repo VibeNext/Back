49be53401ed6b2f982abadde3fdc7fb8d86888b2
gyk <ganada1375@gmail.com>
2025-11-02 21:42:17 +0900

    ✨ SolutionHistory 모델 생성

--- solutions/models.py
+++ solutions/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class SolutionHistory(models.Model):
+    user=models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    mission=models.ForeignKey('missions.Mission', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    solved_at = models.DateTimeField(null=True, blank=True)
+    is_solved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
