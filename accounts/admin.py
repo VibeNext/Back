e768587d7d9b95097a1cfcb2b46f7245b950e385
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-11-07 00:02:21 +0900

    🔨 CustomUserAdmin 추가 (#2)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+from .forms import CustomUserChangeForm, CustomAdminUserCreationForm
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {"fields": ("email","password","name","profile_image",)}),
+        ("Permissions", {"fields": ("is_active","is_staff","is_superuser","groups","user_permissions",)}),
+        ("Important dates", {"fields": ("date_joined","last_login",)}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email","usable_password","password1","password2","name","profile_image",),
+        }),
+    )
+    form = CustomUserChangeForm
+    add_form = CustomAdminUserCreationForm
+    list_display = ("email","is_staff",)
+    search_fields = ("email",)
+    ordering = ("email",)
+
+admin.site.register(User, CustomUserAdmin)
