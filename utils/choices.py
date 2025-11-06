e3a00cc75d8095a6befb5379d6f3f6dce895aedb
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-11-06 17:54:04 +0900

    🔨 MissionCategoryChoices 추가 (#6)

--- utils/choices.py
+++ utils/choices.py
@@ -1,1 +1,6 @@
 from django.db.models import TextChoices, IntegerChoices
+
+class MissionCategoryChoices(TextChoices):
+    SEQUENCE   = 'sequence',   '순차'
+    SELECTION  = 'selection',  '조건'
+    REPETITION = 'repetition', '반복'
