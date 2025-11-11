b89a743b4791d708606b4bf4f1815d6feea1630b
gyk <ganada1375@gmail.com>
2025-11-11 21:28:32 +0900

    🔨 Chat app 생성

--- configs/settings/base.py
+++ configs/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'missions.apps.MissionsConfig',
     'solutions.apps.SolutionsConfig',
+    'chat.apps.ChatConfig',
 ]
 
 MIDDLEWARE = [
