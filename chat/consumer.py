bfee2a4c29c9c265ac360442b00d213c925532dc
gyk <ganada1375@gmail.com>
2025-11-12 14:27:51 +0900

    ✨ Consumer 추가

---
+++ chat/consumer.py
@@ -0,0 +1,20 @@
+from channels.generic.websocket import WebsocketConsumer
+import json
+
+class ChatConsumer(WebsocketConsumer):
+    # 최초 연결
+    def connect(self):
+        self.accept()
+    
+    # 연결 종료        
+    def disconnect(self, close_code):
+        pass
+    
+    # 메세지 전송
+    def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+        
+        self.send(text_data = json.dumps({
+            'message': message
+        }))
