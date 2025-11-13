6f48585c7184e09f7bb1bf8dbbc2a02e1a065b5e
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-11-14 00:49:00 +0900

    🔨 ChapterListSerializer, MissionListSerializer 추가 (#19)

--- missions/serializers.py
+++ missions/serializers.py
@@ -1,1 +1,17 @@
 from rest_framework import serializers
+from .models import Chapter, Mission
+
+class ChapterListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Chapter
+        fields = ('id','title','subtitle',)
+
+class MissionListSerializer(serializers.ModelSerializer):
+    number = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Mission
+        fields = ('id','chapter','number','title','description','image',)
+
+    def get_number(self, obj):
+        return obj.id % 10
