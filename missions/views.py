399d6a40044ee9865c21b90f4616ba59d26f788c
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-11-15 17:41:42 +0900

    🔨 Root 뷰 추가 (#19)

--- missions/views.py
+++ missions/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import HttpRequest
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .services import MissionService
 
-# Create your views here.
+class Root(APIView):
+    def get(self, request:HttpRequest, format=None):
+        mission_service = MissionService(request)
+        data = mission_service.get_list()
+
+        return Response(
+            status=status.HTTP_200_OK,
+            data=data,
+        )
