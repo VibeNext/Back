962c7bacebbb9983475ace7783b99b7b200ec012
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-11-18 01:57:53 +0900

    🐛 비활성화 계정 예외 처리되지 않는 버그 해결 (#21)
    authenticate 메서드는 is_active=False이면 None을 반환한다. 하여 authenticate 메서드로는 비밀번호 불일치와 비활성화 계정을 별도로 예외 처리할 수 없다. 그래서 authenticate 메서드를 제거하고 check_password 메서드 기반으로 교체했다.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,5 +1,7 @@
-from django.contrib.auth import authenticate
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
+
+User = get_user_model()
 
 class LoginSerializer(serializers.Serializer):
     email = serializers.EmailField()
@@ -9,16 +11,16 @@ class LoginSerializer(serializers.Serializer):
         email = attrs.get('email')
         password = attrs.get('password')
 
-        user = authenticate(
-            request=self.context.get('request'),
-            username=email,
-            password=password,
-        )
+        try:
+            user = User.objects.get(email=email)
+        except User.DoesNotExist:
+            raise serializers.ValidationError({'invalid_credentials':'이메일 또는 비밀번호가 일치하지 않아요.'})
 
-        if not user:
-            raise serializers.ValidationError(detail='이메일 또는 비밀번호가 일치하지 않아요.')
+        if not user.check_password(password):
+            raise serializers.ValidationError({'invalid_credentials':'이메일 또는 비밀번호가 일치하지 않아요.'})
+
         if not user.is_active:
-            raise serializers.ValidationError(detail='비활성화된 계정이에요.')
+            raise serializers.ValidationError({'inactive_account':'비활성화된 계정이에요.'})
 
         attrs['user'] = user
         return attrs
--- accounts/views.py
+++ accounts/views.py
@@ -7,7 +7,7 @@ from .services import JWTService
 
 class Login(APIView):
     def post(self, request:HttpRequest, format=None):
-        serializer = LoginSerializer(data=request.data, context={'request': request})
+        serializer = LoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.validated_data['user']
 
