273d8ca94942249adf5b25190f7866ae581001cb
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-11-20 01:00:00 +0900

    ♻️ ChapterService 분리 (#19)

--- missions/services.py
+++ missions/services.py
@@ -2,18 +2,20 @@ from django.http import HttpRequest
 from .models import Chapter, Mission
 from .serializers import ChapterListSerializer, MissionListSerializer
 
-class MissionService:
+class ChapterService:
     def __init__(self, request:HttpRequest):
         self.request = request
 
     def get_list(self):
         chapters = Chapter.objects.all().order_by('id')
-        missions = Mission.objects.all().order_by('id')
-
         chapter_list_serializer = ChapterListSerializer(chapters, many=True)
-        mission_list_serializer = MissionListSerializer(missions, many=True)
+        return chapter_list_serializer.data
 
-        return {
-            "chapter": chapter_list_serializer.data,
-            "mission": mission_list_serializer.data
-        }
+class MissionService:
+    def __init__(self, request:HttpRequest):
+        self.request = request
+
+    def get_list(self):
+        missions = Mission.objects.all().order_by('id')
+        mission_list_serializer = MissionListSerializer(missions, many=True)
+        return mission_list_serializer.data
--- missions/views.py
+++ missions/views.py
@@ -4,7 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAdminUser
 from rest_framework_simplejwt.authentication import JWTAuthentication
-from .services import MissionService
+from .services import ChapterService, MissionService
 
 class Root(APIView):
     authentication_classes = [JWTAuthentication]
@@ -16,14 +16,19 @@ class Root(APIView):
             return [IsAdminUser()]
 
     def get(self, request:HttpRequest, format=None):
-        mission_service = MissionService(request)
+        chapter_service = ChapterService(request)
+        chapter_list = chapter_service.get_list()
 
+        mission_service = MissionService(request)
         if request.user.is_authenticated:
-            data = {'test':'인증됨! 토큰 유효함!'}
+            mission_list = {'test':'인증됨! 토큰 유효함!'}
         else:
-            data = mission_service.get_list()
+            mission_list = mission_service.get_list()
 
         return Response(
             status=status.HTTP_200_OK,
-            data=data,
+            data={
+                "chapter": chapter_list,
+                "mission": mission_list
+            },
         )
