5822e6eeb6995efcc4de16b513957f43391f430d
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-11-19 14:42:02 +0900

    ✨ 회원가입(회원 추가) API 추가 (#25)

--- accounts/services.py
+++ accounts/services.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.http import HttpRequest
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.settings import api_settings
+from utils.decorators.service import validate_data, validate_unique
 from utils.helpers import get_instance_or_404, format_timestamp_iso
 from .serializers import UserSerializer
 
@@ -12,6 +13,12 @@ class UserService:
         self.request = request
         self.instance = get_instance_or_404(User, pk=pk, error_message='사용자를 찾을 수 없어요.') if pk else None
         self.serializer = UserSerializer(self.instance, data=request.data)
+
+    @validate_unique
+    @validate_data
+    def post(self):
+        created_user = self.serializer.save()
+        return created_user
 
 class JWTService:
     def post(self, user):
--- accounts/views.py
+++ accounts/views.py
@@ -2,8 +2,28 @@ from django.http import HttpRequest
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from .serializers import LoginSerializer
-from .services import JWTService
+from .services import UserService, JWTService
+
+class Root(APIView):
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [AllowAny()]
+        else:
+            return [IsAuthenticated()]
+
+    def post(self, request:HttpRequest, format=None):
+        user_service = UserService(request)
+        user = user_service.post()
+
+        jwt_service = JWTService()
+        data = jwt_service.post(user)
+
+        return Response(
+            status=status.HTTP_201_CREATED,
+            data=data,
+        )
 
 class Login(APIView):
     def post(self, request:HttpRequest, format=None):
