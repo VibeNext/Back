b26cfb7be998bde2e92deec39600a64d9d02cafb
Hyerin Sung <hyerinx@ewha.ac.kr>
2025-11-19 22:31:00 +0900

    🔨 인증 헤더 유무에 따라 다른 데이터 응답 (#19)

--- missions/views.py
+++ missions/views.py
@@ -2,12 +2,26 @@ from django.http import HttpRequest
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAdminUser
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .services import MissionService
 
 class Root(APIView):
+    authentication_classes = [JWTAuthentication]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        else:
+            return [IsAdminUser()]
+
     def get(self, request:HttpRequest, format=None):
         mission_service = MissionService(request)
-        data = mission_service.get_list()
+
+        if request.user.is_authenticated:
+            data = {'test':'인증됨! 토큰 유효함!'}
+        else:
+            data = mission_service.get_list()
 
         return Response(
             status=status.HTTP_200_OK,
