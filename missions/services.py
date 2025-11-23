cbdbc0d78186a65c83ef31865350ffa39305d8c1
Hyerin Sung <145767486+hyerindev@users.noreply.github.com>
2025-11-23 11:10:40 +0900

    🔨 학습단계 목록 조회 API 수정: is_unlocked 필드 추가 (#30)
    * 🔨 인증 헤더 유무에 따라 다른 데이터 응답 (#19)
    
    * ♻️ ChapterService 분리 (#19)
    
    * 🔨 로그인 유저의 요청은 응답에 is_unlocked 추가 (#19)
    
    * 🔨 JWT 토큰 검증 실패 시에도 로그인하지 않은 유저처럼 처리 (#19)

--- missions/services.py
+++ missions/services.py
@@ -1,19 +1,58 @@
+from django.db.models import Case, Exists, F, OuterRef, Q, Value, When, BooleanField, IntegerField
+from django.db.models.functions import Floor, Mod
 from django.http import HttpRequest
+from solutions.models import SolutionHistory
 from .models import Chapter, Mission
-from .serializers import ChapterListSerializer, MissionListSerializer
+from .serializers import ChapterListSerializer, MissionListSerializer, MissionListLoginSerializer
+
+class ChapterService:
+    def __init__(self, request:HttpRequest):
+        self.request = request
+
+    def get_list(self):
+        chapters = Chapter.objects.all().order_by('id')
+        chapter_list_serializer = ChapterListSerializer(chapters, many=True)
+        return chapter_list_serializer.data
 
 class MissionService:
     def __init__(self, request:HttpRequest):
         self.request = request
 
     def get_list(self):
-        chapters = Chapter.objects.all().order_by('id')
         missions = Mission.objects.all().order_by('id')
-
-        chapter_list_serializer = ChapterListSerializer(chapters, many=True)
         mission_list_serializer = MissionListSerializer(missions, many=True)
+        return mission_list_serializer.data
 
-        return {
-            "chapter": chapter_list_serializer.data,
-            "mission": mission_list_serializer.data
-        }
+    def get_list_login(self):
+        missions = Mission.objects.annotate(
+            id_mod_10=Mod(F('id'), 10),
+        )
+
+        prev_mission_id = Case(
+            When(id=11, then=Value(None)),
+            When(~Q(id_mod_10=1), then=F('id')-1),
+            When(id_mod_10=1, then=Floor(F('id')/10)*10-10+3),
+            output_field=IntegerField(),
+        )
+
+        prev_mission_is_solved = Exists(
+            SolutionHistory.objects.filter(
+                mission_id=OuterRef('prev_mission_id'),
+                user=self.request.user,
+                is_solved=True,
+            )
+        )
+
+        missions = missions.annotate(
+            prev_mission_id=prev_mission_id,
+            is_unlocked=Case(
+                When(prev_mission_id__isnull=True, then=Value(True)),
+                When(prev_mission_is_solved, then=Value(True)),
+                default=Value(False),
+                output_field=BooleanField(),
+            ),
+        ).all(
+        ).order_by('id')
+
+        mission_list_serializer = MissionListLoginSerializer(missions, many=True)
+        return mission_list_serializer.data
--- missions/views.py
+++ missions/views.py
@@ -2,14 +2,33 @@ from django.http import HttpRequest
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .services import MissionService
+from rest_framework.permissions import AllowAny, IsAdminUser
+from utils.authentication import OptionalJWTAuthentication
+from .services import ChapterService, MissionService
 
 class Root(APIView):
+    authentication_classes = [OptionalJWTAuthentication]
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+        else:
+            return [IsAdminUser()]
+
     def get(self, request:HttpRequest, format=None):
+        chapter_service = ChapterService(request)
+        chapter_list = chapter_service.get_list()
+
         mission_service = MissionService(request)
-        data = mission_service.get_list()
+        if request.user.is_authenticated:
+            mission_list = mission_service.get_list_login()
+        else:
+            mission_list = mission_service.get_list()
 
         return Response(
             status=status.HTTP_200_OK,
-            data=data,
+            data={
+                "chapter": chapter_list,
+                "mission": mission_list
+            },
         )
